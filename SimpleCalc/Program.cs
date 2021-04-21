c5c0d4232e0c997a7d4a82c4457c59c3d2fa512d
Skola Rajak QA <skolarajak.qa@gmail.com>
2021-04-21 23:57:57 +0200

    Refactor code

--- SimpleCalc/Program.cs
+++ SimpleCalc/Program.cs
@@ -8,43 +8,96 @@ namespace SimpleCalc
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static double KorisnickiUnosOperanda()
         {
-            Console.WriteLine("Jednostavni kalkulator");
+            Console.Write("Unesite operand > ");
 
-            double a, b, rezultat = 0;
+            return Convert.ToDouble(Console.ReadLine());
+        }
 
-            Console.Write("Unesite zeljenu operaciju (+, -, *, /) > ");
-            string operacija = Console.ReadLine();
+        public static string KorisnickiUnos(string poruka)
+        {
+            Console.Write("{0} > ", poruka);
 
-            Console.Write("Unesite prvi operand (a) > ");
-            a = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Unesite drugi operand (b) > ");
-            b = Convert.ToDouble(Console.ReadLine());
+            return Console.ReadLine();
+        }
+
+        public static double Izracunaj(string operacija, double operand1, double operand2)
+        {
+            double rez = 0;
 
             switch (operacija)
             {
                 case "+":
-                    rezultat = a + b;
+                    rez = operand1 + operand2;
                     break;
                 case "-":
-                    rezultat = a - b;
+                    rez = operand1 - operand2;
                     break;
                 case "*":
-                    rezultat = a * b;
+                    rez = operand1 * operand2;
                     break;
                 case "/":
-                    rezultat = a / b;
+                    rez = operand1 / operand2;
                     break;
                 default:
                     Console.WriteLine("Nepoznata operacija");
-                    Environment.Exit(1);
                     break;
             }
 
-            //                                              0  1     2          3
-            Console.WriteLine("Rezultat {0} {3} {1} = {2}", a, b, rezultat, operacija);
+            return rez;
+        }
 
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Jednostavni kalkulator");
+
+            const string POTVRDA = "y";
+
+            double a, b, rezultat = 0;
+            string odgovor = POTVRDA;
+
+            while (odgovor == POTVRDA) {
+
+                string operacija = KorisnickiUnos("Unesite zeljenu operaciju (+, -, *, /)");
+
+
+                a = KorisnickiUnosOperanda();
+                b = KorisnickiUnosOperanda();
+
+                rezultat = Izracunaj(operacija, a, b);
+
+                //                                              0  1     2          3
+                Console.WriteLine("Rezultat {0} {3} {1} = {2}", a, b, rezultat, operacija);
+
+                odgovor = KorisnickiUnos("Upisite " + POTVRDA + " da nastavite, bilo sta drugo prekida rad").ToLower();
+            }
+
+
+            int i = 10, j = 10;
+            
+            while (i < 20)
+            {
+                j = 0;
+                while (j < 20) {
+                    Console.WriteLine("[while] Vrednost je {0}", i);
+                    j++;
+                }
+                i++;
+            }
+
+            do
+            {
+                Console.WriteLine("[do..while] Vrednost je {0}", i);
+                i++;
+            } while (i < 20);
+
+            for (int brojac = 0; brojac < 10; brojac++)
+            {
+                Console.WriteLine("[for] Vrednost je {0}", brojac);
+            }
 
             Console.ReadKey();
         }
